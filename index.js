ad70fd1218ef2901713131565844354008fa0700
hofkj331 <>
2021-05-18 01:04:39 +0000

    added variable world tried to move the exclamation mark but it is stuck firmly to the end of hello. also, trying to add 'world!' at the end of const hello created some sort of error.
    Not sure why this all differs so drastically from the lesson

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const hello = require('./hello');
+let world = " world!"
+console.log(hello(),world);
 
-console.log(hello());
+//For the life of me I cannot figure out how your declaration is adding an ! being hello
+//There isn't an exclamation mark anywhere... 
+
+//let hello = 'Hello ';
+//let world = 'world!';
+//console.log(hello,world);
