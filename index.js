ee0743f33bfdf25aec51822c4fc426083db9cb07
hofkj331 <>
2021-06-05 01:35:13 +0000

    the string

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const hello = require('./hello');
-let world = " world!"
-console.log(hello(),world);
-
-//For the life of me I cannot figure out how your declaration is adding an ! being hello
-//There isn't an exclamation mark anywhere... 
-
-//let hello = 'Hello ';
-//let world = 'world!';
-//console.log(hello,world);
+const hello = ('Hello World!');
+console.log(hello);
